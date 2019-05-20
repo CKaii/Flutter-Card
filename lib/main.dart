7080d300ce047bdad716a9da0c10c603660f52c1
unknown <kevin.cheung.kai@gmail.com>
2019-05-20 11:45:57 -0400

    Added contact information and github information

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,70 @@ class MyApp extends StatelessWidget {
                     color: Colors.white,
                     fontWeight: FontWeight.bold),
               ),
-              Text('WEB DEVELOPER',
-                  style: TextStyle(
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20,
-                      color: Colors.blue[200],
-                      letterSpacing: 2.5,
-                      fontWeight: FontWeight.bold))
+              Text(
+                'WEB DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20,
+                    color: Colors.blue[200],
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.phone, color: Colors.teal),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '240 515 0462',
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.email, color: Colors.teal),
+                    SizedBox(width: 10),
+                    Text(
+                      'kevin.cheung.kai@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.computer, color: Colors.teal),
+                      SizedBox(width: 10),
+                      Text(
+                        'https://github.com/CKaii',
+                        style: TextStyle(
+                            color: Colors.teal,
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 20),
+                      )
+                    ],
+                  ))
             ],
           ),
         ),
